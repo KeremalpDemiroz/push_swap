b2f0de2a66cb35f3146942b01a67ed81d2a0de33
Keremalp Demiroz <kedemiro@Keremalp.localdomain>
2025-11-12 02:23:40 +0300

    12-11-25

--- push_swap.c
+++ push_swap.c
@@ -1,6 +1,11 @@
 #include "push_swap.h"
 #include <stdio.h>
 
+void	pexit(/*add pointers needs to be free*/)
+{
+	ft_putendl_fd("Error!", 2);
+	exit(EXIT_FAILURE);
+}
 int	ft_atoi(const char *nptr)
 {
 	long int		sign;
@@ -16,30 +21,22 @@ int	ft_atoi(const char *nptr)
 			sign *= -1;
 		nptr++;
 	}
+	// ft_printf("%s\n", nptr);
 	while ((*nptr) >= '0' && (*nptr) <= '9')
-	{ 
+	{
+		// ft_printf("digit=== %c\n", *nptr);
 		result = (result * 10) + ((*nptr) - 48);
 		nptr++;
 	}
+	if (result * sign > INT_MAX || result * sign < INT_MIN)
+		pexit();
 	return ((int)result * sign);
 }
-void	pexit()
-{
-	ft_putendl_fd("Error!", 2);
-	exit(EXIT_FAILURE);
-}
 
-void	get_list(char	**av)
+void	args_is_int(char **av, int  i)
 {
-	
-}
-
-void	args_is_int(char **av)
-{
-	int	i ;
 	int	j;
 
-	i = 1;
 	while (av[i])
 	{
 		j = 0;
@@ -58,14 +55,74 @@ void	args_is_int(char **av)
 		i++;
 	}
 }
+/*
+	This function will combine multiple args with quote
+	./push_swap "1 2 5 12 5" "2323 52 21" ' 23 332'  4  93
+	to this:
+
+	av[1] = "1 2 5 12 5 2323 52 21 23 332 4 93"
+	
+*/
+// char *compact_args(int ac, char **av)
+// {
+// 	char	*result;
+// 	int		i;
+// 	int		all_size;
+
+// 	i = 1;
+// 	all_size = 0;
+// 	while (i <ac)
+// 	{
+// 		all_size += ft_strlen(av[i]);
+// 		i++;
+// 	}
+// 	i = 1;
+// 	result = calloc(sizeof(char), all_size + 1 + ac -2);
+// 	if (!result)
+// 		return (NULL);
+// 	while (i < ac)
+// 	{
+// 		ft_strlcat(result, av[i], ft_strlen(result) + ft_strlen(av[i]) + 1);
+// 		if (i != ac -1)
+// 			ft_strlcat(result, " ", ft_strlen(result) + 2);
+// 		i++;
+// 	}
+// 	return (result);
+// }
+void	index_sort(t_list args_list, char **av)
+{
+	
+}
+
+int	get_argc(char **split)
+{
+	int	i;
+
+	i = 0;
+	if (!split)
+		return (-1);
+	while (split[i])
+		i++;
+	return (i);
+}
 
 int	main(int ac, char **av)
 {
-	int i = 1;
-	while (av[i])
+	int 	i;
+	char	**args;
+	int		argc;
+
+	i = 1;
+	if (ac == 2)
 	{
-		args_is_int(av);
-		ft_printf("%d\n", ft_atoi(av[i]));
+		args = ft_split(av[1], ' ');
+		ac = get_argc(args);
+		i--;
+	}
+	args_is_int(args, i);
+	while (i < ac)
+	{
+		ft_printf("%d\n", ft_atoi(args[i]));
 		i++;
 	}
 }
